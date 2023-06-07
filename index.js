127853e6d71f4f57c68818f73fe57d25d743925d
Ameya <adengane@gmai.com>
2023-06-07 19:26:50 +0530

    db working

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express")
 
-const PORT = 1337
+const PORT = 5500
 
 const app = express();
+
+const bodyParser = require("body-parser")
+
+const cors = require('cors');
+app.use(cors());
 
 const mongoose = require('mongoose');
 
@@ -23,15 +28,52 @@ db.once('open', () => {
 
 app.use(express.urlencoded({express:true}));
 
-app.use(express.static("public"));
-
+app.use(express.static('public'));
+app.use(bodyParser.json())
 app.use(express.json());
 
-app.use("/api", (req, res) => {
-    console.log("hii");
+app.post("/submit", (req, res) => {
+
+  let link = req.body.link;
+
+  let data = {
+      "link": link,
+      
+  }
+
+  db.collection('users').insertOne(data,(err,collection)=>{
+      if(err){
+          throw err;
+      }
+      console.log("Record Inserted Successfully");
+  });
+
+  // return res.redirect('signup_success.html')
+
+    console.log("lol");
     res.send("Received");
   });
 
 app.listen(PORT, ()=>{
     console.log("Success PORT", PORT)
 });
+
+// Define a route to handle the POST request
+// app.post('/submit', (req, res) => {
+//     // Extract the data from the request body
+//     const value = req.body.value;
+  
+//     // Create a new document using Mongoose and save it to the database
+//     const newData = new DataModel({ value: value });
+//     newData.save()
+//       .then(savedData => {
+//         // Return a response indicating success
+//         res.json({ message: 'Data saved successfully!' });
+//       })
+//       .catch(error => {
+//         // Handle any errors that occur during saving
+//         console.error(error);
+//         res.status(500).json({ error: 'An error occurred while saving the data.' });
+//       });
+//   })
+
